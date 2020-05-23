9829e57fa422a6f4f7a0388d2de88a135e913f17
Kenya Kornilov <sa5mli.xd@gmail.com>
2020-05-23 13:12:04 +0500

    ref

--- mitm-warface/Program.cs
+++ mitm-warface/Program.cs
@@ -8,6 +8,9 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 //using ConnectionAnalizer;
 
+// how it work ?
+// etc\hosts -> 127.0.0.1 s1.warface.ru
+
 namespace mitm_warface
 {
     class Program
@@ -56,24 +59,17 @@ namespace mitm_warface
 
         private static void __AcceptConnection(TcpClient clientConnection)
         {
-            
-            Console.WriteLine("XMPP CONNECTION CAPTURE///....");
+            Console.WriteLine("Warface XMPP Connection captured/...");
 
             try
-            {
+            { 
                 var serverConnection = new TcpClient("128.140.170.64", 5222); // ru-alpha
 
                 NetworkStream clientStream = clientConnection.GetStream();
                 NetworkStream serverStream = serverConnection.GetStream();
 
-
-                
-
-
-
                 byte[] buffer = new byte[4096];
                 int readedBytes = 0;
-
 
                 // why not Packet readed ?
                 // buffer readed - 160 bytes, packet read only 148 bytes - not working
@@ -87,10 +83,18 @@ namespace mitm_warface
                 while (true)
                 {
                     packet = __readPacket(serverStream, "server");
-                    packet.Write(clientStream);
 
                     string content = packet.GetContentString();
 
+                    // попытка пропатчить TLS запрос ничего не даст, только ошибки
+                    //if (content.Equals("<stream:features><starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls><mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>WARFACE</mechanism></mechanisms></stream:features>"))
+                    //{
+                    //    content.Replace("<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls>", "");
+                    //    packet.SetContentString(content);
+                    //}
+
+                    packet.Write(clientStream);
+                    
                     // wait packet tls init
                     if (content.Equals("<stream:features><starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'><required/></starttls><mechanisms xmlns='urn:ietf:params:xml:ns:xmpp-sasl'><mechanism>WARFACE</mechanism></mechanisms></stream:features>"))
                     {
@@ -122,10 +126,7 @@ namespace mitm_warface
                 // full tunnelign non controlled data
                 //new Task(() => __Tunnel(clientStream, serverStream, "__client")).Start();
                 //new Task(() => __Tunnel(serverStream, clientStream, "__server")).Start();
-
                 //while (true) { }
-
-
                 
                 __clientStream = new SslStream(clientStream, true);
                 //__clientStream.AuthenticateAsServer(new X509Certificate2("__cert/warface.cer", "300400"));
@@ -141,22 +142,13 @@ namespace mitm_warface
                 
 
 
-               
-
-
                 __serverStream = new SslStream(serverStream, false, __sslValidationCallback, null);
                 __serverStream.AuthenticateAsClient("s1.warface.ru", null, System.Security.Authentication.SslProtocols.Tls, false);
-
 
                 // tls non controlled tunneling
                 new Task(() => __Tunnel(__clientStream, __serverStream, "_client")).Start();
                 new Task(() => __Tunnel(__serverStream, __clientStream, "_server")).Start();
-
-
-
                 while (true) { }
-
-
 
                 // controlled tunneling with drop data
                 new Task(() =>
@@ -164,40 +156,33 @@ namespace mitm_warface
                     while (true)
                     {
                         Packet _packet = __readPacket(__serverStream, "__server");
+
+                        // control packet this
+
                         _packet.Write(__clientStream);
                     }
                 }).Start();
-
-
 
                 new Task(() =>
                 {
                     while (true)
                     {
-                        
                         Packet _packet = __readPacket(__clientStream, "__client");
+
+                        // control packet this
+
                         _packet.Write(__serverStream);
                     }
                 }).Start();
-
-
-
-
-
 
             }
             catch(Exception exception)
             {
                 Console.WriteLine(exception.Message);
             }
-
-
         }
 
-
-
         private static bool __sslValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors) { return true; }
-
 
         private static byte[] __MAGIC = new byte[4];
         private static Packet __readPacket(Stream stream, string nameOf)
